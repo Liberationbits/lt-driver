36be3e34443d9ef90dc9ce340d3ba2468eee3492
rodant <antonio.rodriguez@posteo.de>
2023-11-22 17:54:59 +0100

    takeover of needed code from template app.

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Logo from '$icons/Logo.svelte';
+	import { currentUser } from '$stores/current-user';
+	import CurrentUser from './CurrentUser.svelte';
+	import { GitPullRequest, MagicWand, PersonSimpleRun, Rss, Sparkle } from 'phosphor-svelte';
+</script>
+
+<div class="navbar mb-2 bg-base-100">
+	<div class="navbar-start">
+		<a href="/" class="btn btn-ghost text-xl normal-case px-0 hover:bg-transparent">
+			<Logo class="h-12 w-12" />
+			<h1 class="text-base md:text-xl font-bold">Liefertour - {$currentUser?.profile?.displayName}</h1>
+		</a>
+	</div>
+
+	<div class="navbar-end flex flex-row items-center gap-4">
+		<CurrentUser />
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import ndk from '$stores/ndk';
+	import { onMount } from 'svelte';
+	import '../app.postcss';
+	import { NDKNip07Signer } from '@nostr-dev-kit/ndk';
+	import { currentUser } from '$stores/current-user';
+	import Navbar from '$components/Navbar.svelte';
+	import { Modals, closeModal } from 'svelte-modals'
+	import { fade } from 'svelte/transition';
+	import { GitPullRequest, Rss, Sparkle } from 'phosphor-svelte';
+	
+	onMount(async () => {
+        try {
+            $ndk.connect();
+
+			if (localStorage.getItem('nostr-login') === 'nip07') {
+				await login();
+			}
+        } catch (e) {
+            console.error(`layout error`, e);
+        }
+    });
+
+	async function login() {
+		$ndk.signer = new NDKNip07Signer();
+		$currentUser = await $ndk.signer?.blockUntilReady();
+		$currentUser.ndk = $ndk;
+
+		if ($currentUser) {
+			localStorage.setItem('nostr-login', 'nip07');
+		}
+	}
+
+	$: if ($currentUser) {
+		console.log(`init`);
+	}
+</script>
+
+<Navbar />
+
+<Modals>
+    <div
+        slot="backdrop"
+        class="backdrop z-20 fixed"
+        on:click={closeModal}
+        transition:fade={{ duration: 100 }}></div>
+</Modals>
+
+<div class="mx-auto">
+	<slot />
+</div>
+
+<style lang="postcss">
+	:global(.userCard .userCard--avatar .avatar--image) {
+		@apply w-12 h-12;
+	}
+
+	.backdrop {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+		backdrop-filter: blur(0.15rem);
+        left: 0;
+        background: rgba(0,0,0,0.50)
+    }
+</style>
