e4adb4ad4cfdf8c038f56443b30e2e778a1ff05a
rodant <antonio.rodriguez@posteo.de>
2023-11-24 12:14:01 +0100

    catch eventual fetchProfile errors.

--- src/lib/stores/current-user.ts
+++ src/lib/stores/current-user.ts
@@ -4,8 +4,13 @@ import { derived, writable } from 'svelte/store';
 export const currentUser = writable<NDKUser | undefined>(undefined);
 
 export const profileName = derived(currentUser, async ($currentUser, set) => {
-    await $currentUser?.fetchProfile();
-    const name = $currentUser?.profile?.name;
-    set(name ? ' - ' + name : '');
+	try {
+		await $currentUser?.fetchProfile();
+		const name = $currentUser?.profile?.name;
+		set(name ? ' - ' + name : '');
+	} catch (error) {
+		console.error('Error fetching user profile: ' + error);
+		set('');
+		// todo: additionally show error to user
+	}
 });
-
