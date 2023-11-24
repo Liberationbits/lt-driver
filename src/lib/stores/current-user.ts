0720b6ee0e95c2dc103dd200887f6ef7aa717bbd
rodant <antonio.rodriguez@posteo.de>
2023-11-24 18:09:51 +0100

    removed unused modal and fixed svelte type error.

--- src/lib/stores/current-user.ts
+++ src/lib/stores/current-user.ts
@@ -3,14 +3,24 @@ import { derived, writable } from 'svelte/store';
 
 export const currentUser = writable<NDKUser | undefined>(undefined);
 
-export const profileName = derived(currentUser, async ($currentUser, set) => {
-	try {
-		await $currentUser?.fetchProfile();
-		const name = $currentUser?.profile?.name;
-		set(name ? ' - ' + name : '');
-	} catch (error) {
-		console.error('Error fetching user profile: ' + error);
-		set('');
-		// todo: additionally show error to user
-	}
-});
+export const profileName = derived(
+	currentUser,
+	($currentUser, set) => {
+		if ($currentUser) {
+			$currentUser
+				.fetchProfile()
+				.then(() => {
+					const name = $currentUser?.profile?.name;
+					set(name ? ' - ' + name : '');
+				})
+				.catch((error) => {
+					console.error('Error fetching user profile: ' + error);
+					set('');
+					// todo: additionally show error to user
+				});
+		} else {
+			set('');
+		}
+	},
+	''
+);
