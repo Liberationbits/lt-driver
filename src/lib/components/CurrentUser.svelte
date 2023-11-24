a5d011633e39d39bb9a9e5b179e48f9f920602de
rodant <antonio.rodriguez@posteo.de>
2023-11-24 12:45:48 +0100

    clean up warnings.

--- src/lib/components/CurrentUser.svelte
+++ src/lib/components/CurrentUser.svelte
@@ -9,17 +9,3 @@
 {:else}
 	<LoginDropdown />
 {/if}
-
-<style lang="postcss">
-	.menu {
-		@apply absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-base-300 py-1 text-base-content shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none;
-	}
-
-	li {
-		@apply block px-4 py-2 text-sm  font-medium hover:bg-base-200 hover:text-gray-800;
-	}
-
-	li a:hover {
-		@apply !bg-transparent;
-	}
-</style>
