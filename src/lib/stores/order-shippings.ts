12fcf10e5b327c3b094c10be8dde15cc4fc46623
rodant <antonio.rodriguez@posteo.de>
2023-11-30 11:38:05 +0100

    Removes unused imports.

--- src/lib/stores/order-shippings.ts
+++ src/lib/stores/order-shippings.ts
@@ -1,5 +1,3 @@
-import type PickupHub from '$lib/model/pickup-hub';
-import { initialHubs } from '$lib/stores/pickup-hubs';
 import type OrderShipping from '$lib/model/order-shipping';
 import { writable, type Writable } from 'svelte/store';
 
