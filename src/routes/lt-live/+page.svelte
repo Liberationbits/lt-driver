6991fe553f8591940dd3dc5edc877c70c5ab6234
rodant <antonio.rodriguez@posteo.de>
2023-12-15 16:29:24 +0100

    show pickup hubs in live tour.

--- src/routes/lt-live/+page.svelte
+++ src/routes/lt-live/+page.svelte
@@ -1,7 +1,35 @@
+<script>
+	import { pickupHubs } from '$stores/pickup-hubs';
+</script>
+
 <sctipt lang="ts"> </sctipt>
 
 <svelte:head>
 	<title>Liefertour Live - App für die Liefertour Verfolgung</title>
 </svelte:head>
 
-<div>Liefertour Live</div>
+<div class="overflow-x-auto">
+	<table class="table">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th>St.</th>
+				<th>M</th>
+				<th>P</th>
+				<th>Zeit</th>
+				<th>Kommentar</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each $pickupHubs as hub}
+				<tr>
+					<td>{hub.code}</td>
+					<td>{hub.membersCount}</td>
+					<td>{hub.portions}</td>
+					<td></td>
+					<td></td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
