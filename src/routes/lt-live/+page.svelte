499e07976e7bbd4f0e9a17d0ba0ef4804a14e5cc
rodant <antonio.rodriguez@posteo.de>
2023-12-18 14:14:03 +0100

    read and display data for tour live page.

--- src/routes/lt-live/+page.svelte
+++ src/routes/lt-live/+page.svelte
@@ -1,8 +1,33 @@
-<script>
+<script lang="ts">
+	import type { ShippingState } from '$lib/model/order-shipping';
+	import { orderShippingAndEventsStore } from '$stores/order-shippings';
 	import { pickupHubs } from '$stores/pickup-hubs';
-</script>
 
-<sctipt lang="ts"> </sctipt>
+	type HubShipping = {
+		hubCode: string;
+		members: number;
+		portions: number;
+		state: ShippingState;
+		time: number;
+		comment: string;
+	};
+
+	$: knownOrderShippingAndEvents = $orderShippingAndEventsStore.filter((ose) =>
+		$pickupHubs.find((hub) => hub.id == ose.orderShipping.customerId)
+	);
+
+	$: hubShippings = knownOrderShippingAndEvents.map<HubShipping>((ose) => {
+		const hub = $pickupHubs.find((h) => h.id == ose.orderShipping.customerId);
+		return {
+			hubCode: hub?.code!,
+			members: hub?.membersCount!,
+			portions: hub?.portions!,
+			state: ose.orderShipping.shippingState(),
+			time: ose.event.created_at ? ose.event.created_at : 0,
+			comment: ose.orderShipping.comment
+		};
+	});
+</script>
 
 <svelte:head>
 	<title>Liefertour Live - App für die Liefertour Verfolgung</title>
@@ -14,20 +39,20 @@
 		<thead>
 			<tr>
 				<th>St.</th>
-				<th>M</th>
-				<th>P</th>
+				<th>M|P</th>
+				<th>Status</th>
 				<th>Zeit</th>
 				<th>Kommentar</th>
 			</tr>
 		</thead>
 		<tbody>
-			{#each $pickupHubs as hub}
+			{#each hubShippings as hs}
 				<tr>
-					<td>{hub.code}</td>
-					<td>{hub.membersCount}</td>
-					<td>{hub.portions}</td>
-					<td></td>
-					<td></td>
+					<td>{hs.hubCode}</td>
+					<td>{hs.members}|{hs.portions}</td>
+					<td>{hs.state}</td>
+					<td>{hs.time}</td>
+					<td>{hs.comment}</td>
 				</tr>
 			{/each}
 		</tbody>
