11ee81ce5049dd330c34968a375e91971572bbf8
Antonio Rodriguez <rodant68@gmail.com>
2024-01-02 17:50:01 +0100

    publish metadata event.

--- src/lib/components/modals/UserProfileEditorModal.svelte
+++ src/lib/components/modals/UserProfileEditorModal.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	import ModalWrapper from '$components/ModalWrapper.svelte';
+	import { currentUser } from '$stores/current-user';
+	import { closeModal } from 'svelte-modals';
 
     export let userName: string;
     export let imageURL: string | undefined = undefined;
     export let isOpen: boolean = false;
+
+    async function updateProfile() {
+        await $currentUser?.fetchProfile();
+        if ($currentUser?.profile) {
+            $currentUser.profile.name = userName;
+            $currentUser.profile.image = imageURL;
+            await $currentUser.publish();
+            closeModal();
+        }
+    }
 </script>
 
 {#if isOpen}
@@ -12,6 +24,6 @@
     <input id="user-name" bind:value="{userName}" placeholder="Name">
     <label for="user-avatar">Profilbild-URL:</label>
     <input id="user-avatar" bind:value="{imageURL}" placeholder="URL">
-    <button class="btn btn-primary mt-2">Speichern</button>
+    <button class="btn btn-primary mt-2" on:click={updateProfile}>Speichern</button>
 </ModalWrapper>
 {/if}
