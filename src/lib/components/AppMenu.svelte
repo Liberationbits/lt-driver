28e4513013b533150beecf37439511a3b302cb02
Antonio Rodriguez <rodant68@gmail.com>
2024-02-05 21:33:30 +0100

    add links to code and terms and conditions.

--- src/lib/components/AppMenu.svelte
+++ src/lib/components/AppMenu.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import CollapsableDropdown from '$lib/components/CollapsableDropdown.svelte';
-	import SubtleButton from './buttons/SubtleButton.svelte';
-	import { Truck, House } from 'phosphor-svelte';
+	import { Truck, House, GitBranch, Scales, Code } from 'phosphor-svelte';
 	import { version } from '$app/environment';
 
 	function donate(): void {}
@@ -14,13 +13,29 @@
 
 	<ul class="menu rounded-box w-[245px] divide-y divide-neutral-800 p-0">
 		<li><a href="/"><House class="h-4 w-4" />Liefertour-Fahrer</a></li>
-		<li>Version: {version}</li>
-		<li>Copyright</li>
-		<li>Über diese App</li>
-		<div class="p-[22px]">
-			<SubtleButton handleClick={donate} class="w-full">
-				<span slot="btn-content">Spenden</span>
-			</SubtleButton>
-		</div>
+		<li><div><GitBranch size={20} /><span>Version: {version}</span></div></li>
+		<li>
+			<a target="_blank" href="https://github.com/Liberationbits/lt-driver">
+				<Code size={20} /> Apps Quellcode
+			</a>
+		</li>
+		<!---li>
+			<a target="_blank" href="https://github.com/Liberationbits/lt-driver">
+				<HandHeart size={20} /> Unterstützen
+			</a>
+		</li>
+		<li>
+			<a target="_blank" class="link" href="/">
+				<PaypalLogo size={20} />PayPal
+			</a>
+		</li>
+		<li>
+			<a target="_blank" class="link" href="weatheredwind420@getalby.com">
+				<Lightning size={20} />Bitcoin Lightling ⚡
+			</a>
+		</li--->
+		<li>
+			<a target="_blank" href="/terms-conditions.html"> <Scales size={20} />Nutzungsbedingungen</a>
+		</li>
 	</ul>
 </CollapsableDropdown>
